89fad00542c010a1676dda30d888048fa56e4a64
InimitablePioneer <seungwonpioneer@gmail.com>
2025-01-01 23:43:20 +0900

    Jan1 at home via MAC

---
+++ src/main/java/com/test/kakaotest/KakaotestApplication.java
@@ -0,0 +1,15 @@
+package com.test.kakaotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaotestApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/test/kakaotest/KakaotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.kakaotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
