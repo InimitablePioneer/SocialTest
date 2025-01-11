2c0d8b87e8afbb37106c03f2f5db35cd0039fff3
InimitablePioneer <seungwonpioneer@gmail.com>
2025-01-11 22:29:11 +0900

    JAN11 at home via MAC

--- src/main/java/com/test/kakaotest/common/login/service/KakaoLoginService.java
+++ src/main/java/com/test/kakaotest/common/login/service/KakaoLoginService.java
@@ -3,7 +3,10 @@ package com.test.kakaotest.common.login.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Service
 public class KakaoLoginService {
@@ -11,7 +14,14 @@ public class KakaoLoginService {
     @Value("${kakao.token-uri}")
     private String tokenUri; //토큰을 요청하는 uri 이때 auth 코드와 함게 보내주어야한다
 
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect_url}")
+    private String redirectUri;
+
     public void login(String code) {
+
         getOauth(code);
     }
 
@@ -20,9 +30,26 @@ public class KakaoLoginService {
 
         //setting the Header
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType
+        headers.setContentType(MediaType.valueOf("application/x-www-form-urlencoded;charset=UTF-8"));
+
+        //send Oauth request to kakao server
+
+
+
+
 
     }
+
+    private MultiValueMap<String, Object> setOauthBody(String code) {
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<String, Object>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", code);
+        return body;
+    }
+
+
 
 
 }
--- src/main/java/com/test/kakaotest/common/util/RestApiRequest.java
+++ src/main/java/com/test/kakaotest/common/util/RestApiRequest.java
@@ -1,6 +1,24 @@
 package com.test.kakaotest.common.util;
 
-public class RestApiRequest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+public class RestApiRequest {
+    public static String request(HttpMethod method, String url, MultiValueMap<String, Object> body, HttpHeaders headers) {
+        RestTemplate restTemplate = new RestTemplate();
+        //request Message
+        HttpEntity<?> requestMessage = new HttpEntity<>(body, headers);
+        //request to kakao server by post method that we made already
+        HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
+        // parsing the response that we get from kakao server
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
+
+    }
 
 }
